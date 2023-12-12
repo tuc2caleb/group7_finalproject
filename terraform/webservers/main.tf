c516704e861061e8333ebedf51d14ae58e80a59b
Caleb <okon.caleb@gmail.com>
2023-12-12 13:08:17 -0500

    Edit workflow

--- terraform/webservers/main.tf
+++ terraform/webservers/main.tf
@@ -1,6 +1,6 @@
 # Module to deploy basic networking for terraform1
 module "aws_webserver_m" {
-  source              = "../../modules/m_webserver"
+  source              = "../../modules/aws_webserver"
   env                 = var.env
   instance_type       = var.instance_type
   prefix              = var.prefix
