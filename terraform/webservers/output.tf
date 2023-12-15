bbab04658a9a31b6d0441208fae2209bd2bdb6f7
Rabia Khalid <39117847+rkhalid7890@users.noreply.github.com>
2023-12-15 01:17:52 +0000

    modified terraform webservers main.tf to clean up extra comments

--- terraform/webservers/output.tf
+++ terraform/webservers/output.tf
@@ -1,57 +1,24 @@
-# # Step 10 - Add output variables
-# output "public_ip" {
-#   value = aws_instance.bastion_host.public_ip
-# }
-
-# output "web_eip" {
-#   value = aws_eip.static_eip.public_ip
-# }
 
 # Step 10 - Add output variables
+# public IP for bastion
 output "public_ip_for_bastion" {
   value = module.aws_webserver_m.public_ip_for_bastion
 }
 
-# output "web_eip" {
-#   value = module.aws_webserver_m.aws_eip.static_eip.public_ip
-# }
-
+# webserver 1 ip address
 output "webserver_vm1_subnet1"{
-  
-//value = aws_instance.public_webservers_vm1.public_ip
 value = module.aws_webserver_m.webserver_vm1_subnet1
 }
-
-
-///
-
-
+# webserver 3 and 4 ip addresses
 output "public_ip_webservers_vm3_vm4" {
- // value = aws_instance.public_webservers_vm3_vm4[*].public_ip
+
   value = module.aws_webserver_m.public_webservers_vm3_vm4
 }
-
-
+# private vm 5 and 6 private ip addresses
 output "private_ip_webservers_vm5_vm6" {
-  //value = aws_instance.web_server_private[*].public_ip
+
    value = module.aws_webserver_m.web_server_private
 }
-
-# output "public_ip" {
-#   value = aws_instance.bastion_host.public_ip
-# }
-
-# output "public_webservers_vm3_vm4"{
-  
-# value = aws_instance.public_webservers_vm3_vm4.public_ip
-# }
-
-
-
-# output "web_server_private"{
-  
-# value = aws_instance.web_server_private.public_ip
-# }
 
 
 
