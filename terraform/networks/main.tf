1dd0310c74e48c3c1c0c8a6a15b6780eb54466b4
Rabia Khalid <39117847+rkhalid7890@users.noreply.github.com>
2023-12-15 01:08:57 +0000

    modified terraform network main.tf to clean up extra comments

--- terraform/networks/main.tf
+++ terraform/networks/main.tf
@@ -1,7 +1,6 @@
 # Module to deploy basic networking 
 module "vpc-dev" {
   source = "../../modules/aws_network"
-  # source             = "git@github.com:rkhalid7890/aws_network.git"
   env                 = var.env
   vpc_cidr            = var.vpc_cidr
   public_cidr_blocks  = var.public_subnet_cidrs
